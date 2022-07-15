2382df7e430e13a9ea4173101bc684accc6d6a50
Lanyard <40360728+Lanyard31@users.noreply.github.com>
2022-07-15 23:21:17 +0200

    Moved first version of car to Main

--- My project/Assets/FollowPlayer.cs
+++ My project/Assets/FollowPlayer.cs
@@ -9,6 +9,7 @@ public class FollowPlayer : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
     public Transform target;
     Camera camera;
+    public bool CameraFollowEnabled = false;
 
     private void Awake()
     {
@@ -18,14 +19,16 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target)
+        if (CameraFollowEnabled == true)
         {
-            Vector3 point = camera.WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-            Vector3 destination = transform.position + delta;
-            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            if (target)
+            {
+                Vector3 point = camera.WorldToViewportPoint(target.position);
+                Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+                Vector3 destination = transform.position + delta;
+                transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            }
         }
-
     }
 }
 
