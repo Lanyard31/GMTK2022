5f8f95a2cb0febec8e2de0e497426e90de590919
Lanyard <40360728+Lanyard31@users.noreply.github.com>
2022-07-16 02:42:10 +0200

    16 Rotations

--- My project/Assets/SpriteSwapper.cs
+++ My project/Assets/SpriteSwapper.cs
@@ -25,55 +25,71 @@ public class SpriteSwapper : MonoBehaviour
     {
         player = FindObjectOfType<Player>();
         rot = player.rotationAngle;
-        if (rot > 0 && rot < 15)
+        if (rot > 0 && rot < 11.25)
         {
             spriteRenderer.sprite = spritelist[0];
         }
-        if (rot > 15 && rot <= 45)
+        if (rot > 11.25 && rot <= 33.75)
         {
             spriteRenderer.sprite = spritelist[1];
         }
-        if (rot > 45 && rot <= 75)
+        if (rot > 33.75 && rot <= 56.25)
         {
             spriteRenderer.sprite = spritelist[2];
         }
-        if (rot > 75 && rot <= 105)
+        if (rot > 56.25 && rot <= 78.75)
         {
             spriteRenderer.sprite = spritelist[3];
         }
-        if (rot > 105 && rot <= 135)
+        if (rot > 78.75 && rot <= 101.25)
         {
             spriteRenderer.sprite = spritelist[4];
         }
-        if (rot > 135 && rot <= 165)
+        if (rot > 101.25 && rot <= 123.75)
         {
             spriteRenderer.sprite = spritelist[5];
         }
-        if (rot > 165 && rot <= 195)
+        if (rot > 123.75 && rot <= 146.25)
         {
             spriteRenderer.sprite = spritelist[6];
         }
-        if (rot > 195 && rot <= 225)
+        if (rot > 146.25 && rot <= 168.75)
         {
             spriteRenderer.sprite = spritelist[7];
         }
-        if (rot > 225 && rot <= 255)
+        if (rot > 168.75 && rot <= 191.25)
         {
             spriteRenderer.sprite = spritelist[8];
         }
-        if (rot > 255 && rot <= 285)
+        if (rot > 191.25 && rot <= 213.75)
         {
             spriteRenderer.sprite = spritelist[9];
         }
-        if (rot > 285 && rot <= 315)
+        if (rot > 213.75 && rot <= 236.25)
         {
             spriteRenderer.sprite = spritelist[10];
         }
-        if (rot > 315 && rot <= 345)
+        if (rot > 236.25 && rot <= 258.75)
         {
             spriteRenderer.sprite = spritelist[11];
         }
-        if (rot > 345 && rot <= 360)
+        if (rot > 258.75 && rot <= 281.25)
+        {
+            spriteRenderer.sprite = spritelist[12];
+        }
+        if (rot > 281.25 && rot <= 303.75)
+        {
+            spriteRenderer.sprite = spritelist[13];
+        }
+        if (rot > 303.75 && rot <= 326.25)
+        {
+            spriteRenderer.sprite = spritelist[14];
+        }
+        if (rot > 326.25 && rot <= 348.75)
+        {
+            spriteRenderer.sprite = spritelist[15];
+        }
+        if (rot > 348.75 && rot <= 360)
         {
             spriteRenderer.sprite = spritelist[0];
         }
